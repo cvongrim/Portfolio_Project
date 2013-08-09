5c4d40860f04db7b7dbdd56eb84a25a609a2810a
Colin <colinvong@gmail.com>
2013-08-09 17:05:12 -0400

    Implemented Image Caching and cleaned up the code

---
+++ xiikPortfolio/src/main/java/com/colinv/xiikportfolio/ImageView.java
@@ -0,0 +1,11 @@
+package com.colinv.xiikportfolio;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: ColinV
+ * Date: 8/9/13
+ * Time: 11:19 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class ImageView {
+}
